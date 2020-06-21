f3ab1aa9d956a4ddff4a4b58015a66dd7c4a36c5
Duy Minh Vo <vo@campus.tu-berlin.de>
2020-06-21 12:22:53 +0200

    Fix fund_contract.js script

--- app/scripts/fund-contract.js
+++ app/scripts/fund-contract.js
@@ -11,10 +11,26 @@ const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
 const payment = process.env.TRUFFLE_CL_BOX_PAYMENT || '1000000000000000000'
 
 module.exports = async callback => {
-  const mc = await MyContract.deployed()
-  const tokenAddress = await mc.getChainlinkToken()
-  const token = await LinkToken.at(tokenAddress)
-  console.log('Funding contract:', mc.address)
-  const tx = await token.transfer(mc.address, payment)
-  callback(tx.tx)
+  // Modified from the exmaple code since it did not work out of the box...
+  try {
+    const accounts = await web3.eth.getAccounts()
+    const defaultAccount = accounts[0]
+
+    const mc = await MyContract.deployed()
+    const tokenAddress = await mc.getChainlinkToken()
+    LinkToken.setProvider(web3.currentProvider)
+    const token = await LinkToken.at(tokenAddress)
+    console.log('Funding contract:', mc.address)
+    const tx = await token.transfer(mc.address, payment, {from: defaultAccount})
+
+    const balance = await token.balanceOf.call(mc.address)
+    console.log(`LINK balance of MyContract: ${balance.toString()}`)
+
+    const totalSupply = await token.totalSupply.call()
+    console.log(`Total LINK supply: ${totalSupply.toString()}`)
+
+    callback(tx.tx)
+  } catch (ex) {
+    console.error(ex)
+  }
 }
