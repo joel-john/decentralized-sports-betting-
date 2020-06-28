4399b6812b08dfaa929ecf14ff9195a452c294b9
Roland Elias Walter Odorfer <odorfer@campus.tu-berlin.de>
2020-06-28 13:30:06 +0200

    Add script for private chainlink node

--- app/truffle-config.js
+++ app/truffle-config.js
@@ -2,6 +2,11 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 module.exports = {
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
     cldev: {
       host: '127.0.0.1',
       port: 8545,
