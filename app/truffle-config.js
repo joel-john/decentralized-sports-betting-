4d622f443bb19b372ac827e2345809e90122eae0
Roland Elias Walter Odorfer <odorfer@campus.tu-berlin.de>
2020-06-28 14:33:54 +0200

    Change wrong port

--- app/truffle-config.js
+++ app/truffle-config.js
@@ -2,11 +2,6 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
-    },
     cldev: {
       host: '127.0.0.1',
       port: 8545,
